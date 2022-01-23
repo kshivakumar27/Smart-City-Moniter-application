7386df39bd790837b70079d4f80cdd2c001a85de
Shivakumar k <56855151+kshivakumar27@users.noreply.github.com>
2022-01-23 21:22:28 +0530

    Add files via upload

--- smartpark/urls.py
+++ smartpark/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
-
+#from django.conf.urls.static import static
 
 urlpatterns = [
     path('', include('park.urls')),
@@ -24,3 +24,4 @@ urlpatterns = [
 ]
 
 
+#urlpatterns = urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
